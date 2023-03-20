1cecb79a8df493b227e1295840291c35f30d41c5
mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
2023-03-19 21:18:12 -0500

    Intento 2

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -48,12 +48,12 @@ public class Anfibio extends Animal{
         return "saltar";
     }
 
-	public static Anfibio crearSalmon(String nombre, int edad, String genero){
+	public static Anfibio crearRana(String nombre, int edad, String genero){
 		ranas ++;	
 		return new Anfibio(nombre, edad, "selva", genero,"rojo",true); 	
 	}
 	
-	public static Anfibio crearBacalao(String nombre, int edad, String genero) {
+	public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
 		salamandras ++;
 		return new Anfibio(nombre, edad, "selva", genero,"negro y amarillo",false); 
     }
